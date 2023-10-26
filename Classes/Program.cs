d8f4e9438efc298ceadd52380f43efd3041b827a
Murat Çarpar <muratcarpar00@gmail.com>
2023-10-26 11:33:27 +0300

    .

---
+++ Classes/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _7_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+
+            ProductManager productManager = new ProductManager();
+
+            customerManager.Add();
+
+            customerManager.Update();
+
+            productManager.Add();
+
+            productManager.Update();
+           
+        }
+    }
+
+    class CustomerManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Added");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Updated");
+        }
+    }
+
+    class ProductManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Added");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Updated");
+        }
+    }
+}
